157fb03b95e1e4c124af26f2951ce13e69062ad6
Virginija <virginijajnlt@gmail.com>
2021-04-07 12:32:53 +0100

    Initial commit

---
+++ Amount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeApp
+{
+    class Amount
+    {
+        public static decimal Change(decimal currency1, decimal currency2, decimal money)
+        {
+            decimal amount = currency1 / currency2 * money;
+            return amount;
+        }
+    }
+}
---
+++ Currency.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeApp
+{
+    class Currency
+    {
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+
+
+    }
+}
